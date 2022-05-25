827f52d17368d33162bd58828df939c8303dc2bd
Maxim Viktorov <victorov_maxim@mail.ru>
2022-05-25 11:41:23 +0300

    dev

---
+++ src/main/java/localhost/sharedRepository/SharedRepositoryApplication.java
@@ -0,0 +1,13 @@
+package localhost.sharedRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/localhost/sharedRepository/SharedRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package localhost.sharedRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
